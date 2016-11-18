9e1e6ecd2541d0727c93fc1b11323a51aa3060eb
Hannah Hamavid <hamavid@Hannahs-MacBook-Pro.local>
2016-11-17 18:59:11 -0800

    changes to slideshow appearance in portfolio page

--- hannah/js/overlay_and_slideshow_fxns.js
+++ hannah/js/overlay_and_slideshow_fxns.js
@@ -1,4 +1,4 @@
-// Script to open and close sidenav
+// Open and close sidenav
 function w3_open() {
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("overlaySidenav").style.display = "block";
@@ -9,25 +9,129 @@ function w3_close() {
     document.getElementById("overlaySidenav").style.display = "none";
 }
 
-// Script to scroll through slides starting at the clicked-on image that triggers the slideshow opening
+
+// -------- Control slideshow appearance and scroll through specified slides -------- // 
+$(document).ready(function(){
+
+// set height of image(s) in slides based on window size
+  function set_slide_height() { 
+    $window = $(window);
+    height = $window.height();
+    var windowwidth=window.innerWidth || 
+    document.documentElement.clientWidth || 
+    document.body.clientWidth;
+    if (height <= 450 || windowwidth <= 450) {var multiplier = 0.98;
+      console.log('h=' + height + 'm=' + multiplier + 'w=' + windowwidth);}
+    else{var multiplier = 0.72;}
+    $('.slide').css('max-height',height*multiplier); // overlay-content height minus padding(?)
+   }
+  $(window).bind('resize', set_slide_height);
+  set_slide_height();
+
+// extract window dimensions and infer left and right areas for clicking back and forward
+  function reset_areas() {
+    $window = $(window);
+     var windowwidth=window.innerWidth || 
+    document.documentElement.clientWidth || 
+    document.body.clientWidth;
+    /*windowwidth = $window.width();*/
+    tenpct = windowwidth/10;
+    height = $window.height();
+    $('#leftside').css('margin-right',tenpct*8);
+    $('#rightside').css('margin-left',tenpct*2);
+    $('#leftside').css('width',tenpct*2);
+    $('#rightside').css('width',tenpct*8);
+    $('#leftside').css('height',0.8*height);
+    $('#rightside').css('height',0.8*height);
+  }
+
+// reset when window is resized (so you don't have to refresh)
+  $(window).resize(reset_areas); 
+  reset_areas();
+
+// highlight left and right scroll arrows when hovering on diff areas of screen
+  $('#leftside').mouseenter(function(){
+    $('.leftscroller').css('opacity','1');
+  });
+  $('#leftside').mouseleave(function(){
+    $('.leftscroller').css('opacity','0.3');
+  });
+  $('#rightside').mouseenter(function(){
+    $('.rightscroller').css('opacity','1');
+  });
+  $('#rightside').mouseleave(function(){
+    $('.rightscroller').css('opacity','0.3');
+  });
+});
+
+// Increment up or down slides
 var slideIndex;
+var x;
 function plusDivs(n) {
+    initial = 0;
     showDivs(slideIndex +=n);
 }
 
+// Show the slide we're on, hide all others
 function showDivs(n) {
     var i;
+    var width;
     slideIndex = n;
-    var x = document.getElementsByClassName("mySlides");
+    x = document.getElementsByClassName("mySlides");
     if (n > x.length) {slideIndex = n % x.length}
     if (n < 1) {slideIndex = x.length};
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
     }
-    x[slideIndex-1].style.display = "block"; 
+    x[slideIndex-1].style.display = "block";
+    get_margins();
+    $(window).bind('resize', get_margins);
 }
 
-// Script to open and close slideshow when an image is clicked
+// Determine margins for this image so it is centered
+function get_margins() {
+  // first set margins to zero so image is rendered at full size
+  var margin_side = 0;
+  var margin_top = 0;
+  reset_margins(margin_top,margin_side);
+
+ // find out how big image is and how big leftover screen is
+    $window = $(window);
+    var windowwidth=window.innerWidth || 
+    document.documentElement.clientWidth || 
+    document.body.clientWidth;
+    /*windowwidth = $window.width();*/
+    if (windowwidth > 992) {overlaywidth = windowwidth - 180} // adjust for sidebar
+    else {overlaywidth = windowwidth}
+    imgwidth = x[slideIndex-1].clientWidth
+    if (imgwidth == 0) { // wait for image to load before setting margins
+      setTimeout(function(){
+          console.log('waiting for image to load');
+          showDivs(slideIndex);
+      }, 100);
+    }
+    margin_side = (overlaywidth - imgwidth)/2;
+    windowheight = $window.height();
+    if (windowheight <= 450 || windowwidth <= 450){var multiplier = 1}else{var multiplier = 0.8}
+    overlayheight = multiplier*windowheight
+    imgheight = x[slideIndex-1].clientHeight;
+    margin_top = (overlayheight - imgheight)/2;
+    
+  // set margins to center the image according to how much screen is leftover
+    reset_margins(margin_top,margin_side);
+}
+
+// pulling this out for less repitition
+function reset_margins(top,side) {
+  var margin_top = top;
+  var margin_side = side;
+  x[slideIndex-1].style.removeProperty('margin-left');
+  x[slideIndex-1].style.setProperty('margin-left', margin_side + 'px', 'important');
+  x[slideIndex-1].style.removeProperty('margin-top');
+  x[slideIndex-1].style.setProperty('margin-top', margin_top + 'px', 'important');
+}
+
+// Open and close slideshow when an image is clicked
 function openSlides() {
     document.getElementById("slideshow").style.display = "block";
     //document.getElementById("overlaySlideshow").style.width = "100%";
@@ -36,7 +140,6 @@ function closeSlides() {
     document.getElementById("slideshow").style.display = "none";
     //document.getElementById("overlaySlideshow").style.width = "0%";
 }
-
 
 
 // ------------------ TOGGLE CENTRAL!! ---------------------//
@@ -74,6 +177,11 @@ function tog_plusminus(x) {
   }
 }
 
+// Change window close/expand
+function tog_closeopen_icon(div_id) {
+    var thisid = $("#"+div_id);
+    $(thisid).toggleClass("fa fa-angle-double-down fa-lg fa fa-angle-double-up fa-lg");
+}
 
 // Change right arrow icon to down arrow icon
 function tog_icon(div_id) {
@@ -86,20 +194,20 @@ function tog_icon(div_id) {
 // so far only works with div ids as numerical 
 // but maybe there's away around that with proper quoting
 function slidetoggle(div_id) {
-    var thisid = $("#"+div_id);
-    $(document).ready(function(){
-        $(thisid).slideToggle();
-    });
+  var thisid = $("#"+div_id);
+  $(document).ready(function(){
+      $(thisid).slideToggle();
+  });
 }    
 
 // GET IMAGE DIMENSIONS! not currently particularly useful or used
 function imgdim(url) {
     var img = new Image();
     img.onload = function(){
-      var height = img.height;
-      var width = img.width;
+      var imgheight = img.height;
+      var imgwidth = img.width;
 
-      console.log("width is " + width + ", height is "+height)
+      console.log("width is " + imgwidth + ", height is " + imgheight)
     }
     img.src = url;
 }
--- js/landing_page_hoverclicks.js
+++ js/landing_page_hoverclicks.js
@@ -13,7 +13,8 @@ $(document).ready(function(){
 		windowwidth = $window.width();
 		height = $window.height();
 		windowhalfwidth = windowwidth/2;
-		imgwidth = (307/442) * height;
+		imgwidth = (307/442) * height; // old version w wider photo
+		/// imgwidth = (267/440) * height; // new version w cropped photo
 		if (windowwidth < imgwidth) {truewidth = windowwidth;} 
 		else {truewidth = imgwidth;}
 		imghalfwidth = truewidth/2;
@@ -24,46 +25,94 @@ $(document).ready(function(){
 		$('#aviva_area').css('margin-top',fivepct*3);
 		$('#aviva_area').css('width',imghalfwidth);
 		$('#aviva_area').css('height',fivepct*17);
-		$('#hannah_area').css('margin-left',windowhalfwidth);
+		$('#hannah_area').css('margin-left',windowhalfwidth-1);
 		$('#hannah_area').css('margin-top',fivepct*2);
-		$('#hannah_area').css('width',imghalfwidth);
+		$('#hannah_area').css('width',imghalfwidth+1);
 		$('#hannah_area').css('height',fivepct*17);
+		wait=0;// milliseconds to delay after clicking area (more on mobile)
+		if (windowwidth < 600) {wait=500;}
 	}
 
+/* CANVAS VERSION OF THINGS */ /*
+// canvas set up and ellipses
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	
+	function ellipse(context, cx, cy, rx, ry){
+        context.save(); // save state
+        context.beginPath();
+
+        context.translate(cx-rx, cy-ry);
+        context.scale(rx, ry);
+        context.arc(1, 1, 1, 0, 2 * Math.PI, false);
+
+        context.restore(); // restore to original state
+        context.stroke();
+	}
+	function clearcanvas(context) {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	}
+
+// reset when window is resized (so you don't have to refresh)
 	$(window).resize(reset_dims); 
 	reset_dims();
-	
-// specify picture to show on mouseover of different divs
+
+
+// show appropriate ovals on mouseenter/leave
 	$('#aviva_area').mouseenter(function(){
 		$('#aviva_area').css('cursor','pointer');
-	  	$('#default').css('opacity','0');
-		$('#hl_aviva').css('opacity','1');
-		$('#hl_hannah').css('opacity','0');
+		ellipse(ctx, (267/4)-5, (440/20*11.5)-3, (267/4)-5, (440/20*8.5)+2); // aviva
 	});
 	$('#aviva_area').mouseleave(function(){
-	  	$('#default').css('opacity','1');
+		clearcanvas(ctx);
+	});
+	$('#hannah_area').mouseenter(function(){
+		$('#hannah_area').css('cursor','pointer');
+		ellipse(ctx, (3*267/4)-4, (440/20*10.5)-3, (267/4)+5, (440/20*8.5)-5); // hannah
+	});
+	$('#hannah_area').mouseleave(function(){
+		clearcanvas(ctx);
+	});	
+*/
+	
+// reset when window is resized (so you don't have to refresh)
+	$(window).resize(reset_dims); 
+	reset_dims();
+
+
+// specify picture to show on mouseover of different divs
+	$('#aviva_area').mouseenter(function(){
+		$('#hl_aviva').css('opacity','1');
+		document.getElementById('hl_aviva').style.WebkitTransition = 'opacity 1s';
+		document.getElementById('hl_aviva').style.MozTransition = 'opacity 1s';
+		document.getElementById('hl_aviva').style.transition = 'opacity 1s';
+	});
+	$('#aviva_area').mouseleave(function(){
 		$('#hl_aviva').css('opacity','0');
 		$('#hl_hannah').css('opacity','0');
 	});
 	$('#hannah_area').mouseenter(function(){
-		$('#hannah_area').css('cursor','pointer');
-	  	$('#default').css('opacity','0');
-		$('#hl_aviva').css('opacity','0');
 		$('#hl_hannah').css('opacity','1');
+		document.getElementById('hl_hannah').style.WebkitTransition = 'opacity 1s';
+		document.getElementById('hl_hannah').style.MozTransition = 'opacity 1s';
+		document.getElementById('hl_hannah').style.transition = 'opacity 1s';
 	});
 	$('#hannah_area').mouseleave(function(){
-	  	$('#default').css('opacity','1');
 		$('#hl_aviva').css('opacity','0');
 		$('#hl_hannah').css('opacity','0');
 	});
 
 
-// Go to relevant subpages when divs are clicked
+// Go to relevant subpages when divs are clicked (can add delay in milliseconds)
 	$("#aviva_area").on('click', function(){
-	     window.location = "./aviva";    
+		setTimeout(function() {
+        	window.location = "./aviva";
+        }, wait);
 	});
 	$("#hannah_area").on('click', function(){
-	     window.location = "./hannah";    
+	     setTimeout(function() {
+        	window.location = "./hannah";
+        }, wait);   
 	});
 	
 });
